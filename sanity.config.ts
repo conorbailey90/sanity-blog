a385eb94677818e79b718d91bba1d1697980460c
conorbailey90 <conbailey90@gmail.com>
2023-09-24 20:06:12 +0100

    sanity config updated

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,25 +3,12 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-// export default defineConfig({
-//   name: 'default',
-//   title: 'conors-blog',
-
-//   projectId: 's7aujfns',
-//   dataset: 'production',
-//   basePath: '/studio',
-//   plugins: [deskTool(), visionTool()],
-
-//   schema: {
-//     types: schemaTypes,
-//   },
-// })
-
 export default defineConfig({
-  name: process.env.SANITY_STUDIO_NAME,
-  title: process.env.SANITY_STUDIO_TITLE,
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
-  dataset: process.env.SANITY_STUDIO_DATASET!,
+  name: 'default',
+  title: 'conors-blog',
+
+  projectId: 's7aujfns',
+  dataset: 'production',
   basePath: '/studio',
   plugins: [deskTool(), visionTool()],
 
@@ -29,3 +16,16 @@ export default defineConfig({
     types: schemaTypes,
   },
 })
+
+// export default defineConfig({
+//   name: process.env.SANITY_STUDIO_NAME,
+//   title: process.env.SANITY_STUDIO_TITLE,
+//   projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+//   dataset: process.env.SANITY_STUDIO_DATASET!,
+//   basePath: '/studio',
+//   plugins: [deskTool(), visionTool()],
+
+//   schema: {
+//     types: schemaTypes,
+//   },
+// })
